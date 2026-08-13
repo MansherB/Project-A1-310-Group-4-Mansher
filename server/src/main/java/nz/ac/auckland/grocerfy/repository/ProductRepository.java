87b7b28c2e9fbeebefbd2dd27b8eeec026a5fd09
WayneTian0929 <waynetian0929@gmail.com>
2026-08-13 20:41:22 +1200

    Basket price comparison implemented (#35)
    * Comparision of cart prices across supermarkets added with UI
    
    * Backend task of finding the cheapest price in basket complete
    
    * Fixed underlines in BasketComparisonControllerTest
    
    * Cart display implemented
    
    * Basket price comparison implemented

--- server/src/main/java/nz/ac/auckland/grocerfy/repository/ProductRepository.java
+++ server/src/main/java/nz/ac/auckland/grocerfy/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package nz.ac.auckland.grocerfy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.List;
+
 import nz.ac.auckland.grocerfy.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
